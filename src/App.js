d82576de082981f73b2e3be1d821841b7d697b48
Aniket Agarwal <aniketggn1@gmail.com>
2021-08-16 11:05:00 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// Modules
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home/Home';
+
+// Components
+import Navbar from './components/Navbar/Navbar';
+
+// Styles
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/" component="" exact />
+        <Route path="/" component="" exact />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from "./Navbar.module.css"
+
+function Navbar() {
+
+    const toggleHamburger = (e) => {
+        const hamburger = document.querySelector(".hamburger");
+        const navLinks = document.querySelector(".navLinks");
+        const links = document.querySelectorAll(".navLinks li");
+
+        navLinks.classList.toggle(styles.open);
+        links.forEach(link => {
+            link.classList.toggle(styles.fade);
+        });
+
+        //Hamburger Animation
+        hamburger.classList.toggle(styles.toggle);
+    }
+
+    return (
+        <nav>
+            <div className={styles.logo}>
+                <img src="" alt="Logo" />
+            </div>
+            <div className={`${styles.hamburger} hamburger`} onClick={toggleHamburger}>
+                <div className={styles.line1}></div>
+                <div className={styles.line2}></div>
+                <div className={styles.line3}></div>
+            </div>
+            <ul className={`${styles.navLinks} navLinks`}>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Solutions</a></li>
+                <li><a href="/">Products</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Contact Us</a></li>
+                <li><button className={styles.loginButton} href="#">Login</button></li>
+                <li><button className={styles.joinButton} href="#">Join</button></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
