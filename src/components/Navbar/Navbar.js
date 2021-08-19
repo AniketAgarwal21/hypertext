9d0e49964036378e59f458d482e51dc7f8592dda
Aniket Agarwal <aniketggn1@gmail.com>
2021-08-19 17:09:56 +0530

    bug fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -30,47 +30,14 @@ function Navbar() {
                 <div className={styles.line3}></div>
             </div>
             <ul className={`${styles.navLinks} navLinks`}>
-                <li><Link to={`${process.env.PUBLIC_URL}/`}>home</Link></li>
-                <li><Link to={`${process.env.PUBLIC_URL}/whoami`}>whoami</Link></li>
-                <li><Link to={`${process.env.PUBLIC_URL}/buyme`}>buyme</Link></li>
-                <li><Link to={`${process.env.PUBLIC_URL}/testimonials`}>testimonials</Link></li>
-                <li><Link to={`${process.env.PUBLIC_URL}/gallery`}>gallery</Link></li>
+                <li><Link to="/">home</Link></li>
+                <li><Link to="/whoami">whoami</Link></li>
+                <li><Link to="/buyme">buyme</Link></li>
+                <li><Link to="/testimonials">testimonials</Link></li>
+                <li><Link to="/gallery">gallery</Link></li>
                 <ThemeToggler />
             </ul>
-        </nav>
-
-        // style={backgroundolor: white !important}
-        // <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" >
-        //     <div className="container-fluid">
-        //         <a className="navbar-brand" href="#">
-        //             Hypertext
-        //         </a>
-        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarContent">
-        //             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        //                 <li className="nav-item">
-        //                     <a className="nav-link active" href="#">About</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">Services</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">Testimonials</a>
-        //                 </li>
-        //             </ul>
-        //             <ul className="navbar-nav">
-        //                 <li className="nav-item">
-        //                     <a className="nav-link active" href="#">Sign Up</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">Login</a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
+        </nav> 
     )
 }
 
